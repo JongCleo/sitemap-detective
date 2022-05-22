11d033ebfcb6cc59afbdc4473e94013340eee487
Leo Kim <leonardkim96@gmail.com>
2022-05-22 01:38:51 -0400

    clear job queue on startup

--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# clean up 
+# clean up and setup
 ./stop.sh
+celery -A celery_worker.celery purge -f
+python ./utilities.py create_db
+brew services start redis
 
-# database set up and run services
-python ./utilities.py create_db &
-brew services start redis &
+# start app, worker and worker dashboard
 flask run &
 watchmedo auto-restart -d 'app/' -p '*.py' -- celery -A celery_worker.celery worker --loglevel=info &
 watchmedo auto-restart -d 'app/' -p '*.py' -- celery --app celery_worker.celery flower --port=5555
+
+# open browsers
 #/usr/bin/open -a "/Applications/Google Chrome.app" 'http://localhost:5555/' &
 #usr/bin/open -a "/Applications/Google Chrome.app" 'http://localhost:5000/'
 
