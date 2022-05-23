58acf16ac6b22d9bfd12f4923b833f8cc6c86057
Leo Kim <leonardkim96@gmail.com>
2022-05-23 14:58:17 -0400

    run celery before flask

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ python ./utilities.py create_db
 brew services start redis
 
 # start app, worker and worker dashboard
-flask run &
 watchmedo auto-restart -d 'app/' -p '*.py' -- celery -A celery_worker.celery worker --loglevel=info &
-watchmedo auto-restart -d 'app/' -p '*.py' -- celery --app celery_worker.celery flower --port=5555
+watchmedo auto-restart -d 'app/' -p '*.py' -- celery --app celery_worker.celery flower --port=5555 &
+flask run
 
 # open browsers
 #/usr/bin/open -a "/Applications/Google Chrome.app" 'http://localhost:5555/' &
